9091122cf1ab5f742eed1a80c5ebc864511f6111
cherryhsu <xcy_cherry@163.com>
2020-05-13 18:06:16 +0800

    富文本编辑

--- admin/src/views/ArticleEdit.vue
+++ admin/src/views/ArticleEdit.vue
@@ -15,8 +15,13 @@
       <el-form-item label="标题">
         <el-input v-model="model.title"></el-input>
       </el-form-item>
-       <el-form-item label="详情">
-        <el-input v-model="model.body"></el-input>
+      <el-form-item label="详情">
+        <VueEditor
+          style="width: 80%"
+          useCustomImageHandler
+          @image-added="handleImageAdded"
+          v-model="model.body"
+        ></VueEditor>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="save">保存</el-button>
@@ -25,9 +30,13 @@
   </div>
 </template>
 <script>
+import { VueEditor } from "vue2-editor";
 export default {
   props: {
     id: {}
+  },
+  components: {
+    VueEditor
   },
   data() {
     return {
@@ -37,9 +46,20 @@ export default {
   },
   created() {
     this.id && this.fetch(); //id存在时执行该方法
-    this.fetchCategoriesOptions()
+    this.fetchCategoriesOptions();
   },
   methods: {
+    async handleImageAdded(file, Editor, cursorLocation, resetUploader) {
+      // An example of using FormData
+      // NOTE: Your key could be different such as:
+      // formData.append('file', file)
+
+      var formData = new FormData();
+      formData.append("file", file);
+      const res = await this.$http.post('upload', formData);
+      Editor.insertEmbed(cursorLocation, "image", res.data.url);
+      resetUploader();
+    },
     async save() {
       let res;
       if (this.id) {
