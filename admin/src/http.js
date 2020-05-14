1941734d01c4649f8c493c3b8fc805f0a3d353d7
cherryhsu <xcy_cherry@163.com>
2020-05-14 17:57:49 +0800

    登录校验

--- admin/src/http.js
+++ admin/src/http.js
@@ -1,7 +1,34 @@
 import axios from 'axios'
+import Vue from 'vue'
+import router from './router'
 
-const http=axios.create({
-    baseURL:"http://localhost:3000/admin/api"
+const http = axios.create({
+    baseURL: "http://localhost:3000/admin/api"
+})
+
+// 添加请求拦截器
+http.interceptors.request.use(function (config) {
+    if (sessionStorage.getItem("token")) {
+        config.headers.Authorization = 'Bearer ' + sessionStorage.token
+    }
+    return config;
+}, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+});
+// 添加响应拦截器
+
+http.interceptors.response.use(res => {
+    return res
+}, err => {//大于400的错进入
+    if (err.response.data.message) {
+        Vue.prototype.$message.error(err.response.data.message)
+
+    }
+    if (err.response.status == 401) {
+        router.push('/login')
+    }
+    return Promise.reject(err)
 })
 
 export default http
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,17 +1,24 @@
 module.exports = app => {
     const express = require('express')
+    const jwt = require('jsonwebtoken')
+    const assert = require('http-assert')
+    const AdminUser = require('../../models/AdminUser')
+    app.set('secret', 'i2kkdadasdsa223')
     const router = express.Router({
         mergeParams: true//合并url参数
     })
     //const Category = require('../../models/Category')
+    //创建
     router.post('/', async (req, res) => {
         const model = await req.Model.create(req.body)//创建
         res.send(model)//将结果发送到前端
     })
+    //查询
     router.post('/:id', async (req, res) => {
         const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)//更新
         res.send(model)//将结果发送到前端
     })
+    //删除
     router.delete('/:id', async (req, res) => {
 
         await req.Model.findByIdAndDelete(req.params.id)//删除
@@ -19,7 +26,16 @@ module.exports = app => {
             success: true
         })//将结果发送到前端
     })
-    router.get('/', async (req, res) => {
+    router.get('/', async (req, res, next) => {
+        console.log("aa" + req.headers.authorization)
+        const token = String(req.headers.authorization || '').split(' ').pop()
+        assert(token, 401, '请先登录')
+        const { id } = jwt.verify(token, app.get('secret'))
+        assert(id, 401, '无效的token')
+        req.user = await AdminUser.findById(id)
+        assert(req.user, 401, '请先登录')
+        await next()
+    }, async (req, res) => {
         const queryOptions = {}
         if (req.Model.modelName === 'Category') {
             queryOptions.populate = 'parent'
@@ -28,6 +44,8 @@ module.exports = app => {
         // const data = await req.Model.find().populate('parent').limit(10)//查询,限制10条,populate关联字段对象
         res.send(data)//将结果发送到前端
     })
+
+    //资源详情
     router.get('/:id', async (req, res) => {
         const data = await req.Model.findById(req.params.id)//查询根据id
         res.send(data)//将结果发送到前端
@@ -42,8 +60,40 @@ module.exports = app => {
     const upload = multer({ dest: __dirname + '/../../uploads' })//当前文件所在文件夹
     app.post('/admin/api/upload', upload.single('file'), async (req, res) => {
         const file = req.file
-        file.url=`http://localhost:3000/uploads/${file.filename}`
+        file.url = `http://localhost:3000/uploads/${file.filename}`
         res.send(file)
 
+    })
+
+    app.post('/admin/api/login', upload.single('file'), async (req, res) => {
+        const { username, password } = req.body
+        //1.根据用户名找用户
+
+        const user = await (await AdminUser.findOne({ username }).select('+password'))
+        assert(user, 422, '用户不存在')
+        // if (!user) {
+        //     return res.status(422).send({
+        //         message: '用户不存在'
+        //     })
+        // }
+        //2.校验密码
+        const isValid = require('bcrypt').compareSync(password, user.password)
+        assert(isValid, 422, '密码错误')
+        // if (!isValid) {
+        //     return res.status(422).send({
+        //         message: '密码错误'
+        //     })
+        // }
+        //3.返回token npm i jsonwebtoken
+        //const jwt = require('jsonwebtoken')
+        const token = jwt.sign({ id: user._id }, app.get('secret'))//获取配置
+        res.send({ token })
+
+    })
+    //错误处理函数(必须有next)
+    app.use(async (err, req, res, next) => {
+        res.status(err.statusCode || 500).send({
+            message: err.message
+        })
     })
 }
